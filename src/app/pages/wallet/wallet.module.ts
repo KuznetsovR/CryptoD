f6c0a37f701df28ee01b9a0bdffae5b05f04b283
KuznetsovR <abcruslan2004@mail.ru>
2022-10-06 21:52:13 +0500

    added page container to every page

--- src/app/pages/wallet/wallet.module.ts
+++ src/app/pages/wallet/wallet.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { WalletRoutingModule } from './wallet-routing.module';
 import { WalletComponent } from './wallet.component';
+import {ButtonModule} from "../../modules/ui/button/button.module";
 
 @NgModule({
   declarations: [WalletComponent],
-  imports: [CommonModule, WalletRoutingModule],
+  imports: [CommonModule, WalletRoutingModule, ButtonModule],
 })
 export class WalletModule {}
